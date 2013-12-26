f87aea5966a40680e14a22e5e7fc28b60810977d
thethreecoffins <thethreecoffins@gmail.com>
2013-12-26 16:13:27 +0800

    v0.7.3.1

--- protected/config/main.php
+++ protected/config/main.php
@@ -84,7 +84,7 @@ return array(
 			),
 		),
 		'db'=>array(
-			'connectionString' => 'mysql:host=127.0.0.1;dbname=wcptf_dev',
+			'connectionString' => 'mysql:host=192.168.1.107;dbname=wcptf_dev',
 			'emulatePrepare' => true,
 			'username' => 'wcadmin',
 			'password' => '123',
